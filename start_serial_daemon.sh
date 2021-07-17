7e69f1b121f59dce94dac5a34dfd1346956ebb33
Josh Silverstone <64409956+jsilversto@users.noreply.github.com>
2021-07-17 22:35:19 +0100

    Fixed PTY port permissions

--- start_serial_daemon.sh
+++ start_serial_daemon.sh
@@ -3,11 +3,14 @@
 TTY=/dev/ttygrbl
 NOHUP=nohup_start_sim.out
 
+# Switch to script directory
+cd "$(dirname "$0")"
+
 # Kill any already-running instances
 sudo killall --quiet --signal SIGTERM grbl-pilase socat
 
 # Start grbl-pilase, piping io through TTY, running in background with nohup
-sudo nohup socat -d -d -d PTY,raw,link=$TTY,echo=0 "EXEC:'./grbl-pilase -n -s step.out -b block.out',pty,raw,echo=0" > $NOHUP 2>&1 &
+sudo nohup socat -d -d -d PTY,raw,link=$TTY,perm=0777,echo=0 "EXEC:'./grbl-pilase -n -s step.out -b block.out',pty,raw,echo=0,perm=0777" > $NOHUP 2>&1 &
 
 # Report results
 echo "GrblHAL (pilase) started successfully"
