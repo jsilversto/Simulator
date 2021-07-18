8324a72bb20d4e785be18f51d1438550f3de7657
Josh Silverstone <64409956+jsilversto@users.noreply.github.com>
2021-07-18 17:35:15 +0100

    Added daemon startup test

--- start_serial_daemon.sh
+++ start_serial_daemon.sh
@@ -2,6 +2,7 @@
 
 TTY=/dev/ttygrbl
 NOHUP=nohup_start_sim.out
+EXE=/home/pi/pilase/grbl-pilase
 
 # Switch to script directory
 cd "$(dirname "$0")"
@@ -10,11 +11,22 @@ cd "$(dirname "$0")"
 sudo killall --quiet --signal SIGTERM grbl-pilase socat
 
 # Start grbl-pilase, piping io through TTY, running in background with nohup
-sudo nohup socat -d -d -d PTY,raw,link=$TTY,perm=0777,echo=0 "EXEC:'./grbl-pilase -n -s step.out -b block.out',pty,raw,echo=0,perm=0777" > $NOHUP 2>&1 &
+sudo nohup socat -d -d -d PTY,raw,link=$TTY,perm=0777,echo=0 "EXEC:'$EXE -n -s step.out -b block.out',pty,raw,echo=0,perm=0777" > $NOHUP 2>&1 &
+
+# Wait for process to start, ports to open
+sleep 0.1
 
 # Report results
-echo "GrblHAL (pilase) started successfully"
-echo "Nohup socat output available at $NOHUP"
-echo "Access grbl-pilase TTY at $TTY"
+if [ "$(ps -a | grep grbl-pilase)" ] ; then
+	echo "GrblHAL (pilase) started successfully"
+	echo "Nohup socat output available at $NOHUP"
+	echo "Access grbl-pilase TTY at $TTY"
+else
+	echo "GrblHAL (pilase) failed to start"
+	echo "This is what happens when I tried:"
+	$EXE
+fi
+
+
 
 
